b2f98e89e0801f73fea63564ff0b575fb4867dad
davelim0208 <37682672+davelim0208@users.noreply.github.com>
2023-10-06 14:05:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+
+# Streamlit UI
+st.title("Simple Streamlit App")
+
+# Text input
+user_name = st.text_input("Enter your name:", "John Doe")
+
+# Button to greet the user
+if st.button("Greet"):
+    st.write(f"Hello, {user_name}!")
+
+# Add more content to your app as needed
+streamlit run simple_streamlit_app.py
 
