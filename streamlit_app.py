7932077ec96989a82bd4f15b59749bb720d9a7ed
davelim0208 <37682672+davelim0208@users.noreply.github.com>
2023-10-06 14:47:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,58 @@
 import streamlit as st
+import openai
+
+# Set your Azure OpenAI API key
+api_key = '250783a2a3ed4cbe93dd0d7d2c443144'
+
+# Initialize the Azure OpenAI client
+openai.api_key = api_key
 
 # Streamlit UI
-st.title("Prompt Generator App")
+st.title("ChatGPT Simulation")
 
-# Input fields for user details
-name = st.text_input("Your Name:", "John Doe")
-location = st.text_input("Your Location:", "New York")
-age = st.number_input("Your Age:", min_value=0, max_value=150, value=30, step=1)
+# Create a conversation history to store user and AI messages
+conversation_history = []
 
-# Button to generate the prompt
-if st.button("Generate Prompt"):
-    # Create a prompt based on user details
-    prompt = f"Name: {name}\nLocation: {location}\nAge: {age}"
-    
-    # Display the generated prompt
-    st.write("Generated Prompt:")
-    st.write(prompt)
+# Function to send a message to ChatGPT and update the conversation history
+def send_message(message):
+    # Add the user's message to the conversation history
+    conversation_history.append({"role": "user", "content": message})
 
+    # Generate a response from ChatGPT
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        messages=conversation_history
+    )
+
+    # Add ChatGPT's response to the conversation history
+    conversation_history.append({"role": "assistant", "content": response.choices[0].message["content"]})
+
+    return response.choices[0].message["content"]
+
+# Chat input field
+user_input = st.text_input("You:", "")
+
+# Send button
+if st.button("Send"):
+    if user_input.strip() != "":
+        user_message = user_input
+        st.text(f"You: {user_message}")
+        
+        # Send the user's message to ChatGPT
+        assistant_message = send_message(user_message)
+        
+        st.text(f"ChatGPT: {assistant_message}")
+        st.text("")
+
+# Clear button to reset the conversation
+if st.button("Clear Conversation"):
+    conversation_history = []
+
+# Display the conversation history
+if len(conversation_history) > 0:
+    st.markdown("## Conversation History")
+    for message in conversation_history:
+        if message["role"] == "user":
+            st.text(f"You: {message['content']}")
+        else:
+            st.text(f"ChatGPT: {message['content']}")
