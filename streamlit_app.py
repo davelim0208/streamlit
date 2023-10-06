1a5e7ba9f86a21ed0bd5a8f97055827d3f641a3f
davelim0208 <37682672+davelim0208@users.noreply.github.com>
2023-10-06 14:37:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,19 @@
 import streamlit as st
-import openai
-
-# Set your Azure OpenAI API key
-api_key = '250783a2a3ed4cbe93dd0d7d2c443144'
-
-# Initialize the Azure OpenAI client
-openai.api_key = api_key
 
 # Streamlit UI
-st.title("Azure OpenAI Prompt Generator")
+st.title("Prompt Generator App")
 
-# Create a text input field for the prompt
-prompt = st.text_area("Enter a prompt:", "Translate the following English text to French: 'Hello, world.'")
+# Input fields for user details
+name = st.text_input("Your Name:", "John Doe")
+location = st.text_input("Your Location:", "New York")
+age = st.number_input("Your Age:", min_value=0, max_value=150, value=30, step=1)
 
-# Create a button to generate a response
-if st.button("Generate Response"):
-    try:
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=50,  # Adjust as needed
-            n=1,
-            stop=None,
-            temperature=0.7  # Adjust for randomness
-        )
-
-        # Display the generated response
-        st.write("Generated Response:")
-        st.write(response.choices[0].text.strip())
-
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
+# Button to generate the prompt
+if st.button("Generate Prompt"):
+    # Create a prompt based on user details
+    prompt = f"Name: {name}\nLocation: {location}\nAge: {age}"
+    
+    # Display the generated prompt
+    st.write("Generated Prompt:")
+    st.write(prompt)
 
